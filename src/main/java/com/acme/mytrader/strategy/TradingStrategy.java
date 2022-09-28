86857c0f58716028c48eb7a1d94389eb42a9c635
Nuwan Sumihiran <sumihiran.nuwan@gmail.com>
2022-09-28 07:12:21 +0530

    feat(#1): adds ability to monitor price movements on a specified stock

--- src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
+++ src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
@@ -1,10 +1,87 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.price.PriceListener;
+import com.acme.mytrader.price.PriceSource;
+
+import java.util.Optional;
+
 /**
+ * A {@link PriceSource} observer that monitors the stock price of its security.
+ *
  * <pre>
  * User Story: As a trader I want to be able to monitor stock prices such
  * that when they breach a trigger level orders can be executed automatically
  * </pre>
+ *
+ * @author Nuwan Sumihiran
  */
-public class TradingStrategy {
+public class TradingStrategy implements PriceListener, AutoCloseable {
+
+    private final String security;
+    private double stockPrice;
+    private PriceSource priceSource;
+
+    /**
+     * Initialize a {@link TradingStrategy} with the given {@code security} name
+     *
+     * @param security the name of the stock
+     */
+    public TradingStrategy(String security) {
+        this.security = security;
+    }
+
+    @Override
+    public void priceUpdate(String security, double price) {
+        if (security.equals(this.security)) {
+            this.stockPrice = price;
+        }
+    }
+
+    /**
+     * Returns security name
+     *
+     * @return the name of the security
+     */
+    public String getSecurity() {
+        return security;
+    }
+
+    /**
+     * Returns observed stock price
+     *
+     * @return current stock price
+     */
+    public Optional<Double> getStockPrice() {
+        return Optional.of(stockPrice);
+    }
+
+    /**
+     * Returns the {@link PriceSource} currently subscribed to
+     *
+     * @return the subscribed {@link PriceSource}
+     */
+    public PriceSource getPriceSource() {
+        return priceSource;
+    }
+
+    /**
+     * Subscribe this strategy to the given {@link PriceSource}. If a {@link PriceSource} already subscribed it will
+     * be unsubscribed.
+     *
+     * @param priceSource a {@link PriceSource} to subscribe
+     */
+    public void setPriceSource(PriceSource priceSource) {
+        if (this.priceSource != null) {
+            this.close();
+        }
+        priceSource.addPriceListener(this);
+        this.priceSource = priceSource;
+    }
+
+    @Override
+    public void close() {
+        if (this.priceSource != null) {
+            this.priceSource.removePriceListener(this);
+        }
+    }
 }
--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,9 +1,39 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.price.PriceSource;
 import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class TradingStrategyTest {
     @Test
-    public void testNothing() {
+    public void tradingStrategySubscribeToPriceListener() {
+        var priceSource = spy(PriceSource.class);
+        try (var tradingStrategy = new TradingStrategy("IBM")) {
+            assertNull(tradingStrategy.getPriceSource());
+
+            tradingStrategy.setPriceSource(priceSource);
+
+            verify(priceSource, times(1)).addPriceListener(tradingStrategy);
+        }
+        verify(priceSource, times(1)).removePriceListener(any());
+    }
+
+    @Test
+    public void monitorStockPriceWhenPriceUpdated() {
+        // Arrange
+        var tradingStrategy = new TradingStrategy("IBM");
+
+        tradingStrategy.priceUpdate("IBM", 121.74);
+        assertEquals(Optional.of(121.74), tradingStrategy.getStockPrice());
+
+        tradingStrategy.priceUpdate("IBM", 120.00);
+        tradingStrategy.priceUpdate("APL", 151.76);
+
+        assertEquals(Optional.of(120.00), tradingStrategy.getStockPrice());
     }
 }
