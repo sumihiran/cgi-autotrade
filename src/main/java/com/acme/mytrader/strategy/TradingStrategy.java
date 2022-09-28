e0640b93273d4dce6c37b5e2aeb518e782d7d6c9
Nuwan Sumihiran <sumihiran.nuwan@gmail.com>
2022-09-28 07:48:33 +0530

    feat(#2): adds automatic order execution when the stock price fall below OR rise above a specific price level

--- src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
+++ src/main/java/com/acme/mytrader/strategy/TradingStrategy.java
@@ -1,12 +1,14 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionService;
 import com.acme.mytrader.price.PriceListener;
 import com.acme.mytrader.price.PriceSource;
 
 import java.util.Optional;
 
 /**
- * A {@link PriceSource} observer that monitors the stock price of its security.
+ * A {@link PriceSource} observer that monitors the stock price of its security and executes orders automatically when
+ * the stock prices breach a particular trigger level.
  *
  * <pre>
  * User Story: As a trader I want to be able to monitor stock prices such
@@ -16,25 +18,151 @@ import java.util.Optional;
  * @author Nuwan Sumihiran
  */
 public class TradingStrategy implements PriceListener, AutoCloseable {
+    private static final String BUY = "buy";
+    private static final String SELL = "sell";
+    private static final String DIRECTION_HIGH = "high";
+    private static final String DIRECTION_LOW = "low";
 
     private final String security;
+
+    private final double triggerLevelPrice;
+    private final String triggerDirection;
+    private final String orderType;
+    private final double orderPrice;
+    private final int orderVolume;
+
+    private double prevTriggeredStockPrice = Double.NEGATIVE_INFINITY;
     private double stockPrice;
     private PriceSource priceSource;
+    private ExecutionService executionService;
 
     /**
-     * Initialize a {@link TradingStrategy} with the given {@code security} name
+     * Initialize a {@link TradingStrategy} for the given {@code security} to place a BUY order when the stock price
+     * drops below the given {@code triggerLevelPrice}.
      *
-     * @param security the name of the stock
+     * @param security          the name of the security
+     * @param triggerLevelPrice the price level to check against
+     * @param buyPrice          the buy order price
+     * @param buyVolume         the buy order volume
+     * @return an initialized {@link TradingStrategy} instance
      */
-    public TradingStrategy(String security) {
+    public static TradingStrategy placeBuyOrderForSecurityWhenPriceBelow(
+            String security, double triggerLevelPrice, double buyPrice, int buyVolume) {
+        return new TradingStrategy(
+                security, triggerLevelPrice, DIRECTION_LOW, BUY, buyPrice, buyVolume);
+    }
+
+    /**
+     * Initialize a {@link TradingStrategy} for the given {@code security} to place a BUY order when the stock price
+     * rise above the given {@code triggerLevelPrice}.
+     *
+     * @param security          the name of the security
+     * @param triggerLevelPrice the price level to check against
+     * @param buyPrice          the buy order price
+     * @param buyVolume         the buy order volume
+     * @return an initialized {@link TradingStrategy} instance
+     */
+    public static TradingStrategy placeBuyOrderForSecurityWhenPriceAbove(
+            String security, double triggerLevelPrice, double buyPrice, int buyVolume) {
+        return new TradingStrategy(
+                security, triggerLevelPrice, DIRECTION_HIGH, BUY, buyPrice, buyVolume);
+    }
+
+    /**
+     * Initialize a {@link TradingStrategy} for the given {@code security} to place a SELL order when the stock price
+     * drops below the given {@code triggerLevelPrice}.
+     *
+     * @param security          the name of the security
+     * @param triggerLevelPrice the price level to check against
+     * @param sellPrice         the sell order price
+     * @param sellVolume        the sell order volume
+     * @return an initialized {@link TradingStrategy} instance
+     */
+    public static TradingStrategy placeSellOrderForSecurityWhenPriceBelow(
+            String security, double triggerLevelPrice, double sellPrice, int sellVolume) {
+        return new TradingStrategy(
+                security, triggerLevelPrice, DIRECTION_LOW, SELL, sellPrice, sellVolume);
+    }
+
+    /**
+     * Initialize a {@link TradingStrategy} for the given {@code security} to place a SELL order when the stock price
+     * rise above the given {@code triggerLevelPrice}.
+     *
+     * @param security          the name of the security
+     * @param triggerLevelPrice the price level to check against
+     * @param sellPrice         the sell order price
+     * @param sellVolume        the sell order volume
+     * @return an initialized {@link TradingStrategy} instance
+     */
+    public static TradingStrategy placeSellOrderForSecurityWhenPriceAbove(
+            String security, double triggerLevelPrice, double sellPrice, int sellVolume) {
+        return new TradingStrategy(
+                security, triggerLevelPrice, DIRECTION_HIGH, SELL, sellPrice, sellVolume);
+    }
+
+    /**
+     * Initialize a {@link TradingStrategy} with the given {@code security}, {@code triggerLevelPrice},
+     * {@code triggerDirection} and executes an order of {@code orderType} with given {@code orderPrice}
+     * and {@code orderVolume}
+     *
+     * @param security          the name of the security
+     * @param triggerLevelPrice the price level to trigger an order
+     * @param triggerDirection  the direction of trigger. either {@code DIRECTION_HIGH} or @{code DIRECTION_LOW}
+     * @param orderType         the type of the order. either {@code BUY} or {@code SELL}
+     * @param orderPrice        the price of the placing order
+     * @param orderVolume       the volume of the placing order
+     */
+    protected TradingStrategy(
+            String security, double triggerLevelPrice, String triggerDirection,
+            String orderType, double orderPrice, int orderVolume) {
         this.security = security;
+        this.triggerLevelPrice = triggerLevelPrice;
+        this.triggerDirection = triggerDirection;
+        this.orderType = orderType;
+        this.orderPrice = orderPrice;
+        this.orderVolume = orderVolume;
     }
 
     @Override
     public void priceUpdate(String security, double price) {
         if (security.equals(this.security)) {
-            this.stockPrice = price;
+            this.onPriceUpdate(price);
         }
+    }
+
+    private void onPriceUpdate(double price) {
+        this.stockPrice = price;
+
+        if (shouldTrigger(price)) {
+            executeOrder();
+            prevTriggeredStockPrice = price;
+        } else if (!isTriggerLevelSatisfied(price) && prevTriggeredStockPrice != Double.NEGATIVE_INFINITY) {
+            prevTriggeredStockPrice = Double.NEGATIVE_INFINITY;
+        }
+    }
+
+    private void executeOrder() {
+        if (executionService == null) {
+            return;
+        }
+
+        if (orderType.equals(BUY)) {
+            executionService.buy(security, orderPrice, orderVolume);
+        } else if (orderType.equals(SELL)) {
+            executionService.sell(security, orderPrice, orderVolume);
+        }
+    }
+
+    private boolean shouldTrigger(double price) {
+        return isTriggerLevelSatisfied(price) && prevTriggeredStockPrice == Double.NEGATIVE_INFINITY;
+    }
+
+    private boolean isTriggerLevelSatisfied(double price) {
+        if (triggerDirection.equals(DIRECTION_HIGH)) {
+            return price > triggerLevelPrice;
+        }
+
+        return price < triggerLevelPrice;
     }
 
     /**
@@ -53,6 +181,51 @@ public class TradingStrategy implements PriceListener, AutoCloseable {
      */
     public Optional<Double> getStockPrice() {
         return Optional.of(stockPrice);
+    }
+
+    /**
+     * Returns the price level to trigger an order
+     *
+     * @return price level to trigger an order
+     */
+    public double getTriggerLevelPrice() {
+        return triggerLevelPrice;
+    }
+
+    /**
+     * Returns the direction of trigger
+     *
+     * @return either {@code DIRECTION_HIGH} or @{code DIRECTION_LOW}
+     */
+    public String getTriggerDirection() {
+        return triggerDirection;
+    }
+
+    /**
+     * Returns the type of the order to be executed
+     *
+     * @return the type of the order. either {@code BUY} or {@code SELL}
+     */
+    public String getOrderType() {
+        return orderType;
+    }
+
+    /***
+     * Returns the price of the placing order
+     *
+     * @return the price of the placing order
+     */
+    public double getOrderPrice() {
+        return orderPrice;
+    }
+
+    /**
+     * Returns the volume of the placing order
+     *
+     * @return the volume of the placing order
+     */
+    public int getOrderVolume() {
+        return orderVolume;
     }
 
     /**
@@ -76,6 +249,24 @@ public class TradingStrategy implements PriceListener, AutoCloseable {
         }
         priceSource.addPriceListener(this);
         this.priceSource = priceSource;
+    }
+
+    /**
+     * Gets the {@link ExecutionService} in use
+     *
+     * @return an optional of {@link ExecutionService}
+     */
+    public Optional<ExecutionService> getExecutionService() {
+        return Optional.of(executionService);
+    }
+
+    /**
+     * Sets the order {@code ExecutionService}
+     *
+     * @param executionService the {@code ExecutionService}
+     */
+    public void setExecutionService(ExecutionService executionService) {
+        this.executionService = executionService;
     }
 
     @Override
--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,8 +1,10 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionService;
 import com.acme.mytrader.price.PriceSource;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -13,7 +15,8 @@ public class TradingStrategyTest {
     @Test
     public void tradingStrategySubscribeToPriceListener() {
         var priceSource = spy(PriceSource.class);
-        try (var tradingStrategy = new TradingStrategy("IBM")) {
+        try (var tradingStrategy =
+                     TradingStrategy.placeSellOrderForSecurityWhenPriceAbove("IBM", 100, 101, 10)) {
             assertNull(tradingStrategy.getPriceSource());
 
             tradingStrategy.setPriceSource(priceSource);
@@ -26,7 +29,8 @@ public class TradingStrategyTest {
     @Test
     public void monitorStockPriceWhenPriceUpdated() {
         // Arrange
-        var tradingStrategy = new TradingStrategy("IBM");
+        var tradingStrategy = TradingStrategy
+                .placeSellOrderForSecurityWhenPriceAbove("IBM", 100, 101, 10);
 
         tradingStrategy.priceUpdate("IBM", 121.74);
         assertEquals(Optional.of(121.74), tradingStrategy.getStockPrice());
@@ -35,5 +39,72 @@ public class TradingStrategyTest {
         tradingStrategy.priceUpdate("APL", 151.76);
 
         assertEquals(Optional.of(120.00), tradingStrategy.getStockPrice());
+    }
+
+    @Test
+    public void shouldTriggerOrderWhenTriggerLevelReached()
+    {
+        var strategy = TradingStrategy
+                .placeBuyOrderForSecurityWhenPriceAbove("IBM", 100, 101,50);
+        var executionService = spy(ExecutionService.class);
+        strategy.setExecutionService(executionService);
+
+        // Act
+        strategy.priceUpdate("IBM", 101.0);
+        strategy.priceUpdate("IBM", 105.0);
+
+        // Assert
+        assertEquals(executionService, strategy.getExecutionService().orElse(null));
+
+        var resultStockPrice = strategy.getStockPrice();
+        assertTrue(resultStockPrice.isPresent());
+        assertEquals(Optional.of(105.0), resultStockPrice);
+        verify(executionService, times(1)).buy("IBM", 101, 50);
+    }
+
+    @Test
+    public void shouldNotTriggerOrderWhenTriggerLevelNotReached()
+    {
+        var strategy = TradingStrategy
+                .placeSellOrderForSecurityWhenPriceBelow("IBM", 100, 99,10);
+        var executionService = spy(ExecutionService.class);
+        strategy.setExecutionService(executionService);
+
+        // Act
+        strategy.priceUpdate("IBM", 100.0);
+        strategy.priceUpdate("IBM", 105.0);
+
+        // Assert
+        var resultStockPrice = strategy.getStockPrice();
+        assertEquals(Optional.of(105.0), resultStockPrice);
+        verify(executionService, times(0)).sell("IBM", 99, 10);
+    }
+
+    @Test
+    public void shouldTriggerOrderWhenTriggerLevelReachedTwice()
+    {
+        var sellStrategy = TradingStrategy
+                .placeSellOrderForSecurityWhenPriceAbove("CGI", 50, 52,10);
+        var buyStrategy = TradingStrategy
+                .placeBuyOrderForSecurityWhenPriceBelow("CGI", 45, 44,100);
+
+        var executionService = spy(ExecutionService.class);
+        sellStrategy.setExecutionService(executionService);
+        buyStrategy.setExecutionService(executionService);
+
+        var prices = Arrays.asList(40, 44, 51, 48, 42, 48, 52, 55, 46);
+
+        // Act
+        prices.forEach(price -> {
+            sellStrategy.priceUpdate("CGI", price);
+            buyStrategy.priceUpdate("CGI", price);
+        });
+
+        // Assert
+        assertEquals(Optional.of(46.0), sellStrategy.getStockPrice());
+        assertEquals(Optional.of(46.0), buyStrategy.getStockPrice());
+
+        verify(executionService, times(2)).sell("CGI", 52, 10);
+        verify(executionService, times(2)).buy("CGI", 44, 100);
     }
 }
